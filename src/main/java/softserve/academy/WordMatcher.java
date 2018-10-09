4895b9d74bec1ea9fbe5924211a03e658dbc0434
Kateryna Kinchak <katrin.kinchak@gmail.com>
2018-10-09 10:25:21 +0300

    Works for numbers 0 - 999.

--- src/main/java/softserve/academy/WordMatcher.java
+++ src/main/java/softserve/academy/WordMatcher.java
@@ -37,6 +37,7 @@ class WordMatcher {
     }
 
     static {
+//        tens[0] = "ноль";
         tens[1] = "десять";
         tens[2] = "двадцать";
         tens[3] = "тридцать";
@@ -49,7 +50,7 @@ class WordMatcher {
     }
 
     static {
-        hundreds[0] = "";
+//        hundreds[0] = "ноль";
         hundreds[1] = "сто";
         hundreds[2] = "двести";
         hundreds[3] = "триста";
@@ -93,8 +94,15 @@ class WordMatcher {
                     return getTwoDigitNumString(num);
                 }
             case 3:
-                return "";
+                if (number % 100 == 0) {
+                    return hundreds[number / 100];
+                } else {
+                    return getThreeDigitNumString(num);
+                }
             case 4:
+//                if (number % 1000 == 0) {
+//                    return
+//                }
                 return "";
             case 5:
                 return "";
@@ -112,12 +120,48 @@ class WordMatcher {
 //        return result;
     }
 
+    private static String getThreeDigitNumString(String num) {
+        StringBuilder strBuilder = new StringBuilder();
+        String[] numComponents = num.split("");
+        int hundredsComponent = Integer.parseInt(numComponents[0]);
+        int tensComponent = Integer.parseInt(numComponents[1]);
+        int digitsComponent = Integer.parseInt(numComponents[2]);
+        if (tensComponent == 0) {
+            strBuilder
+                    .append(hundreds[hundredsComponent])
+                    .append(" ")
+                    .append(digitsComponent == 0 ? "" : digits[digitsComponent]);
+        } else if (digitsComponent == 0) {
+            strBuilder
+                    .append(hundreds[hundredsComponent])
+                    .append(" ")
+                    .append(tens[tensComponent]);
+        } else if (tensComponent == 1) {
+            strBuilder
+                    .append(hundreds[hundredsComponent])
+                    .append(" ")
+                    .append(teens[digitsComponent]);
+        } else {
+            strBuilder
+                    .append(hundreds[hundredsComponent])
+                    .append(" ")
+                    .append(tens[tensComponent])
+                    .append(" ")
+                    .append(digits[digitsComponent]);
+        }
+        return strBuilder.toString();
+    }
+
     private static String getTwoDigitNumString(String num) {
         StringBuilder strBuilder = new StringBuilder();
         String[] numComponents = num.split("");
         int tensComponent = Integer.parseInt(numComponents[0]);
         int digitsComponent = Integer.parseInt(numComponents[1]);
-        strBuilder.append(tens[tensComponent]).append(" ").append(digits[digitsComponent]);
+        strBuilder
+                .append(tens[tensComponent])
+                .append(" ")
+                .append(digits[digitsComponent]);
+
         return strBuilder.toString();
     }
 }
